66b456e55b086c5d130c318395d1d48829607762
Joseelmax-00 <eeseljose@gmail.com>
2023-05-23 22:42:29 -0300

    Add an error message when no channel is found. Add functionality to search for channel name by entering @Vsauce instead of full URL

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ from bs4 import BeautifulSoup
 from config import YOUTUBE_API_KEY
 
 
-def get_video_urls_from_channel(channel_input, api_key=YOUTUBE_API_KEY):
+def get_video_urls_from_channel(channel_input, api_key):
     youtube = build('youtube', 'v3', developerKey=api_key)
     
-    # Regular expression to match both channel ID and username formats
+    if channel_input.startswith('@'):
+        channel_input = 'https://www.youtube.com/' + channel_input
+    
     match = re.match(r'(?:https?://)?(?:www\.)?youtube\.com/(?:channel/|@)?([^/]+)/?', channel_input)
     if not match:
-        print('Invalid YouTube channel URL or ID')
-        return []
+        return None, 'Invalid YouTube channel URL or ID'
 
     channel_id_or_username = match.group(1)
     
@@ -28,32 +29,41 @@ def get_video_urls_from_channel(channel_input, api_key=YOUTUBE_API_KEY):
         if canonical_link:
             channel_id = canonical_link['href'].split('/')[-1]
         else:
-            print('Could not extract channel ID')
-            return []
+            return None, 'Could not extract channel ID'
 
-    try:
-        request = youtube.search().list(
-            part='snippet',
-            channelId=channel_id,
-            maxResults=50,  # This can be increased up to 50 per request
-            type='video'
-        )
-        response = request.execute()
-        
-        video_urls = []
-        for item in response['items']:
-            video_id = item['id']['videoId']
-            video_url = f'https://www.youtube.com/watch?v={video_id}'
-            video_urls.append(video_url)
-            
-        return video_urls
+    video_urls = []
+    page_token = None
+    while True:
+        try:
+            request = youtube.search().list(
+                part='snippet',
+                channelId=channel_id,
+                maxResults=50,
+                type='video',
+                pageToken=page_token
+            )
+            response = request.execute()
 
-    except HttpError as e:
-        print(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
-        return []
+            for item in response['items']:
+                video_id = item['id']['videoId']
+                video_url = f'https://www.youtube.com/watch?v={video_id}'
+                video_urls.append(video_url)
+
+            page_token = response.get('nextPageToken')
+            if not page_token:  # No more pages
+                break
+
+        except HttpError as e:
+            print(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
+            break  # Stop retrieving videos if an error occurs
+
+    return video_urls, None  # No error
 
 channel_input = st.text_input('Enter Youtube channel URL or ID')
 if channel_input:
-    video_list = get_video_urls_from_channel(channel_input, YOUTUBE_API_KEY)
-    st.table(video_list)
+    video_list, error = get_video_urls_from_channel(channel_input, API_KEY)
+    if error:
+        st.error(error)
+    else:
+        st.table(video_list)
 
